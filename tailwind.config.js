9b3bfd870b2e6c2222d77fb6810d363e82d61582
Abid Hussain <80194194+op10y@users.noreply.github.com>
2022-11-10 10:34:48 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "sans": ["Poppins", "Arial", "sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
