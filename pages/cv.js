43ff5d923f2eb02af5d6b924de9760c14098b932
Abid Hussain <80194194+op10y@users.noreply.github.com>
2023-09-22 12:26:47 +0530

    Create cv.js

---
+++ pages/cv.js
@@ -0,0 +1,21 @@
+$('#toggle-box-checkbox').on('change', function(){
+  if(this.checked){
+    $('body').addClass('night');
+  }else{
+    $('body').removeClass('night');
+  }
+});
+
+
+
+
+
+// run demo if in searchresult preview
+function demo(){
+    setInterval(function(){
+      $("#toggle-box-checkbox").trigger('click');
+    }, 1000);
+}
+if (document.location.pathname.indexOf('fullcpgrid')>-1){
+  demo();
+} 
