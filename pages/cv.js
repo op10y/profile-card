72548a9b32fb14e6ec5a378a7224a67c0fb48e67
Abid Hussain <80194194+op10y@users.noreply.github.com>
2023-09-22 15:32:41 +0530

    Update cv.js
    js

--- pages/cv.js
+++ pages/cv.js
@@ -1,20 +1,17 @@
-$('#toggle-box-checkbox').on('change', function(){
-  if(this.checked){
-    $('body').addClass('night');
-  }else{
-    $('body').removeClass('night');
+$("#toggle-box-checkbox").on("change", function () {
+  if (this.checked) {
+    $("body").addClass("night");
+  } else {
+    $("body").removeClass("night");
   }
 });
 
-
-
-
-
 // run demo if in searchresult preview
-function demo(){
-    setInterval(function(){
-      $("#toggle-box-checkbox").trigger('click');
-    }, 1000);
+function demo() {
+  setInterval(function () {
+    $("#toggle-box-checkbox").trigger("click");
+  }, 1000);
 }
-if (document.location.pathname.indexOf('fullcpgrid')>-1){
+if (document.location.pathname.indexOf("fullcpgrid") > -1) {
   demo();
+}
